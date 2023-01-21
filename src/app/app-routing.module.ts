4fc4099f0248526af618ae9e70c6f426869ab6f9
ikenjokudc <ikenjokudc@gmail.com>
2023-01-21 15:16:13 +0100

    create register module angluar cli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'notifications', loadChildren: () => import('./pop-up-notifications/pop-up-notifications.module').then(m => m.PopUpNotificationsModule) }];
+const routes: Routes = [{ path: 'notifications', loadChildren: () => import('./pop-up-notifications/pop-up-notifications.module').then(m => m.PopUpNotificationsModule) }, { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
