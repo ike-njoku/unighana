dd764e0fd5f9ca3eda97e8a9d5456e090603894f
ikenjokudc <ikenjokudc@gmail.com>
2023-01-21 18:43:36 +0100

    added formfield moule angular cli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'notifications', loadChildren: () => import('./pop-up-notifications/pop-up-notifications.module').then(m => m.PopUpNotificationsModule) }, { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }, { path: 'svg', loadChildren: () => import('./register-svg/register-svg.module').then(m => m.RegisterSvgModule) }];
+const routes: Routes = [{ path: 'notifications', loadChildren: () => import('./pop-up-notifications/pop-up-notifications.module').then(m => m.PopUpNotificationsModule) }, { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }, { path: 'svg', loadChildren: () => import('./register-svg/register-svg.module').then(m => m.RegisterSvgModule) }, { path: 'form-field', loadChildren: () => import('./shared/form-field/form-field.module').then(m => m.FormFieldModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
