cba804873ca95c74671362b0e5bcbd7a5569e2b9
ikenjokudc <ikenjokudc@gmail.com>
2023-01-22 21:19:56 +0100

    updates

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const PORT = process.env.PORT || 8905;
 const path = require('path');
 const express = require('express');
-const forceSsl = require('force-ssl-heroku');
 const app = express();
 
-app.use(forceSsl);
 app.use(express.static(__dirname + '/dist/unighana'));
 app.use(express.json());
 
