2faf06559d56a9edb623881e21305a238f3d32b3
ikenjokudc <ikenjokudc@gmail.com>
2023-01-22 20:07:29 +0100

    default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [ { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }, { path: 'loader', loadChildren: () => import('./loader/loader.module').then(m => m.LoaderModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }];
+const routes: Routes = [
+  {path: '', redirectTo: 'register', pathMatch: 'full'},
+  { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }, { path: 'loader', loadChildren: () => import('./loader/loader.module').then(m => m.LoaderModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
